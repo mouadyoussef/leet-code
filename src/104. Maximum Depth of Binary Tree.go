99b89257fe383b62bc78a58fbcb5e162a6894ba3
Mouad YOUSSEF <imouad.you@gmail.com>
2022-03-06 19:33:18 +0100

    104. Maximum Depth of Binary Tree

---
+++ src/104. Maximum Depth of Binary Tree.go
@@ -0,0 +1,22 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	if right > left {
+		return right + 1
+	}
+
+	return left + 1
+}
