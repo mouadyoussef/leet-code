47843cbe3c8124026ebde283cbdbe7e0cda947eb
YoussefMouad <imouad.you@gmail.com>
2022-04-05 16:12:21 +0200

    938. Range Sum of BST

---
+++ src/938. Range Sum of BST.go
@@ -0,0 +1,23 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	return bfs(root, low, high, 0)
+}
+
+func bfs(root *TreeNode, low int, high int, sum int) int {
+	if root == nil {
+		return sum
+	}
+
+	if root.Val < low || root.Val > high {
+		return bfs(root.Left, low, high, sum) + bfs(root.Right, low, high, sum)
+	}
+
+	return bfs(root.Left, low, high, sum) + bfs(root.Right, low, high, sum) + root.Val
+}
