e3a0b76b975c5cc9a1035d3f28200da4b41ce8e1
YoussefMouad <imouad.you@gmail.com>
2022-04-12 10:33:00 +0000

    746. Min Cost Climbing Stairs

---
+++ src/746. Min Cost Climbing Stairs.go
@@ -0,0 +1,19 @@
+package src
+
+func minCostClimbingStairs(cost []int) int {
+	n := len(cost)
+
+	for i := 2; i < n; i++ {
+		if cost[i-1] < cost[i-2] {
+			cost[i] = cost[i] + cost[i-1]
+		} else {
+			cost[i] = cost[i] + cost[i-2]
+		}
+	}
+
+	if cost[n-1] < cost[n-2] {
+		return cost[n-1]
+	} else {
+		return cost[n-2]
+	}
+}
